8bd6a702d6d4714c7365c153c08a85ed50b2408b
rekawba <brekawek9@gmail.com>
2026-03-05 19:01:43 +0100

    Add screenshots after failure and restore baseURL

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,8 @@ export default defineConfig({
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'retain-on-failure',
-        // baseURL: 'https://wtfqsbkm.elementor.cloud/elementor-36/',
+        screenshot: 'only-on-failure',
+        baseURL: 'https://wtfqsbkm.elementor.cloud/elementor-36/',
     },
 
     /* Configure projects for major browsers */
